f08d10cc65a64a0485c712553817b6c094f60f45
unknown <76191429+khai-cotrol@users.noreply.github.com>
2021-04-07 10:38:41 +0700

    toantu

---
+++ toantu.js
@@ -0,0 +1,8 @@
+let inputwidth ;
+let inputheight;
+inputwidth = prompt('ENter the width');
+inputheight = prompt('Enter the height');
+let width = parseInt(inputwidth);
+let  height = parseInt(inputheight);
+let area = width * height ;
+document.write("the area is :" +area);
